f99457cdeb4e594e06cdbaf6ef40c3420a368812
butichex <tdyutin@gmail.com>
2022-04-16 19:25:43 +0300

    init

---
+++ scripts/constants.js
@@ -0,0 +1,20 @@
+var constants = {
+  _mapTitle: 'Interactive Tatarstan Title',
+  _mapSubtitle: 'Interactive Tatarstan Subtitle',
+  _mapLogo: 'Interactive Tatarstan Logo',
+	_authorName: 'Имя автора:',
+	_authorURL: 'Почта автора или веб-сайт:',
+  _codeCredit: 'Автор кода:',
+	_githubRepo: 'Автор репозитория GitHub',
+	_tileProvider: 'Basemap Tiles',
+	_zoomControls: 'Zoom Controls',
+  _narrativeWidth: 'Narrative Width',
+  _imgContainerHeight: 'Media Container Height',
+  _pixelsAfterFinalChapter: 'Pixels After Final Chapter',
+  _narrativeBackground: 'Narrative Background Color',
+  _narrativeText: 'Narrative Text Color',
+  _narrativeActive: 'Active Chapter Background Color',
+  _narrativeLink: 'Narrative Link Color',
+  _googleAnalytics: 'Google Analytics Tracking ID',
+  _enableLightbox: 'Enable Lightbox for Images'
+};
